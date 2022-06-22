979197ce0cf862a69cbfac11958ed4509438d596
Jennifer <jenniferfgriffin@utexas.edu>
2022-06-21 21:49:35 -0500

    act 3 complete

--- act3.sh
+++ act3.sh
@@ -1,13 +1,15 @@
 # 1. Unload the Millenium Falcon in ONE COMMAND!
 # Move the whole crew from the millenium_falcon directory into the death_star directory. HINT: * following a directory will grab all files/folders inside of a directory (directory/*)
-
+mv death_star/millenium_falcon/* death_star
 # 2. darth_vader has defeated obi_wan! Delete poor obi_wan.
-
+rm obi_wan.txt
 # 3.  Our heroes have disabled the tractor beam! Move the whole crew back into the millenium_falcon!
 # Remember: darth_vader remains in the death_star and emperor_palpatine is still in the empire.
+mv death_star/emperor_palpatine.txt empire mv death_star/* death_star/millenium_falcon mv empire/death_star/millenium_falcon/darth_vadar.txt  empire/death_star
 
 # 4. Move the millenium_falcon back into the rebellion directory.
-
+mv star_wars/empire/death_star/millenium_falcon star_wars/rebellion
 # 5. darth_vader leaves the death_star to pursue luke_skywalker! Move him from the death_star into the empire directory!
-
+mv star_wars/empire/death_star/darth_vadar.txt star_wars/rebellion
 # 6. Thanks to his practice back home at Beggar’s Canyon, Luke blew up the death_star! Remove it from the galaxy!
+rm -r death_star
